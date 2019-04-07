fe142539b5756b0cd0e0e4b394d263064e52fd0c
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-07 08:10:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function appendKitten(name){
   return [...kittens, name]
 }
 
-function prependKitten(name){}
+function prependKitten(name){
+  return [name, ...kittens]
+}
 
 function removeLastKitten(){}
 
